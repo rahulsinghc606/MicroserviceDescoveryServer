26f5ac377d298035b5721bd5a40fe1b5d8de3746
rahulsinghc606 <rahulsinghc606@gmail.com>
2021-04-29 18:21:08 +0530

    "First time Commit"

---
+++ src/test/java/com/example/microservicedescoveryserver/MicroserviceDescoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicedescoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDescoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
